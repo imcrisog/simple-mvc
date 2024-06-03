af6c2e095f985587ddf17fa01ee5cf0597eef67e
Fairebol <firuel22@gmail.com>
2024-06-03 04:13:11 -0300

    Navbar Improved in Home

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,4 +1,24 @@
-<h1> Hola desde Home </h1>
+<div class="w-full h-screen">
+    <div class="flex w-full p-2 text-xl justify-left">
+        <div class="flex w-fit my-auto text-center items-center gap-x-1 font-extrabold ">
+            <img class="w-[2.5rem]" src="<?= LOCALHOST ?>/img/eico.png" alt="EICO N°1">
+            <span>INVEICO</span>
+        </div>
+        <div class="flex px-8 items-center text-center flex-row font-medium gap-x-2 [&>*]:rounded-full [&>*]:p-2 [&>*]:px-4 [&>*]:duration-300">
+            <!-- REGISTER FOR DEVELOPER ENVIROMENT ONLY -->
+            <div class="hover:bg-slate-800 transform">
+                <a href="<?= LOCALHOST ?>/register">Register</a>
+            </div>
+            <div class="hover:bg-slate-800 transform">
+                <a href="<?= LOCALHOST ?>/login">Login</a>
+            </div>
+            <div class="hover:bg-slate-800 transform">
+                <a href="<?= LOCALHOST ?>/profile">Profile</a>
+            </div>
+        </div>
+    </div>  
+</div>
+
 
 <!--
 <?php if (count($users) > 0) { ?>
